e4fbc700f056cbf1bb876072e5697f358853589c
Cloud Park <vividxenon@gmail.com>
2019-01-06 00:55:58 +0900

    3주차 코드 추가

--- Materials/Code/3rd assignment_company_info_crawler.py
+++ Materials/Code/3rd assignment_company_info_crawler.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import urllib.request
+import csv
 
 
 def get_soup(page):
@@ -43,12 +44,36 @@ def get_lables_and_values(company_info_soup):
     lable_value_dict = {}
     return lable_value_dict
 
+def get_spec(spec_info_soup):
+    speclist = spec_info_soup.find('div', class_='specListWrap')
+    specitems = speclist.find_all('div', class_='item')
+    for spec in specitems:
+        item = spec.find('strong', class_='tit').get_text().split("   ")
+        item_name = item[0].strip()
+        item_percent = item[-1].replace(" 보유\n", " ").strip()
+        average_item_score = spec.find('span', class_='score').get_text().strip()
+        print(item_name, item_percent, average_item_score)
+    return
+
+def save(dict):
+    with open('names.csv', 'w', newline='') as csvfile:
+        fieldnames = ['first_name', 'last_name']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+        writer.writeheader()
+        writer.writerow({'first_name': 'Baked', 'last_name': 'Beans'})
+
 
 if __name__ == '__main__':
-    company_info_url = 'http://www.jobkorea.co.kr/Company/1845948/?C_IDX=5871'
-    company_info_soup = get_soup_by_page(company_info_url)
+    company_info_basis = 'http://www.jobkorea.co.kr/Company/'
+    company_number = '1845948/'
+    target_company_url = company_info_basis + company_number
+    company_info_soup = get_soup_by_page(target_company_url)
     lable_value_dict = get_lables_and_values(company_info_soup)
     company_name = get_company_name(company_info_soup)
 
-    print(company_name)
-    print(lable_value_dict)
+    spec_info_url = f'{target_company_url}PassAvgSpec'
+
+    spec_info_soup = get_soup_by_page(spec_info_url)
+    get_spec(spec_info_soup)
+    save(lable_value_dict)
